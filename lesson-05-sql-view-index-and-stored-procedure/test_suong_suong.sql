252bb775eb1b836dc9e7919c7a7619bb27dfa9ac
doanhuong1992vn <doanhuong1992vn@gmail.com>
2023-04-13 00:34:04 +0700

    update

--- lesson-05-sql-view-index-and-stored-procedure/test_suong_suong.sql
+++ lesson-05-sql-view-index-and-stored-procedure/test_suong_suong.sql
@@ -1,5 +1,7 @@
 USE QUAN_LY_BAN_HANG;
-SELECT DISTINCT CUSTOMER.NAME 
-FROM CUSTOMER 
-JOIN the_order WHERE the_order.customer_id = customer.id
-GROUP BY CUSTOMER.NAME;
+SELECT DISTINCT NAME AS "List of customer bought",
+				COUNT(the_order.id) AS "Number of orders"
+	FROM CUSTOMER 
+		JOIN the_order 
+			WHERE the_order.customer_id = customer.id
+				GROUP BY NAME;
